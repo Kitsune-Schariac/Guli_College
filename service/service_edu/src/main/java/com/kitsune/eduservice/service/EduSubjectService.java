f3a407246156559a55dbc6cf1865d79964f11040
Kitsune <1852139224@qq.com>
2021-12-30 19:17:08 +0800

    add edu_subject service, controller, entity, mapper

---
+++ service/service_edu/src/main/java/com/kitsune/eduservice/service/EduSubjectService.java
@@ -0,0 +1,16 @@
+package com.kitsune.eduservice.service;
+
+import com.kitsune.eduservice.entity.EduSubject;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 课程科目 服务类
+ * </p>
+ *
+ * @author Kitsune
+ * @since 2021-12-30
+ */
+public interface EduSubjectService extends IService<EduSubject> {
+
+}
---
+++ service/service_edu/src/main/java/com/kitsune/eduservice/service/impl/EduSubjectServiceImpl.java
@@ -0,0 +1,20 @@
+package com.kitsune.eduservice.service.impl;
+
+import com.kitsune.eduservice.entity.EduSubject;
+import com.kitsune.eduservice.mapper.EduSubjectMapper;
+import com.kitsune.eduservice.service.EduSubjectService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 课程科目 服务实现类
+ * </p>
+ *
+ * @author Kitsune
+ * @since 2021-12-30
+ */
+@Service
+public class EduSubjectServiceImpl extends ServiceImpl<EduSubjectMapper, EduSubject> implements EduSubjectService {
+
+}
