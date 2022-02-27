d198fe7ff3f80d726e8fa106f774ecffac0e327f
kitsune <1852139224@qq.com>
2022-02-27 09:59:30 +0800

    finished upload file to oss

--- service/service_oss/src/main/java/com/kitsune/oss/service/OssService.java
+++ service/service_oss/src/main/java/com/kitsune/oss/service/OssService.java
@@ -1,4 +1,7 @@
 package com.kitsune.oss.service;
 
+import org.springframework.web.multipart.MultipartFile;
+
 public interface OssService {
+    String uploadFileAvatar(MultipartFile file);
 }
--- service/service_oss/src/main/java/com/kitsune/oss/service/impl/OssServiceImpl.java
+++ service/service_oss/src/main/java/com/kitsune/oss/service/impl/OssServiceImpl.java
@@ -1,8 +1,77 @@
 package com.kitsune.oss.service.impl;
 
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClientBuilder;
 import com.kitsune.oss.service.OssService;
+import org.joda.time.DateTime;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.InputStream;
+import java.util.UUID;
+
+import static com.kitsune.oss.utils.ConstanPropertiesUtils.*;
 
 @Service
 public class OssServiceImpl implements OssService {
+
+    //上传文件到oss
+    @Override
+    public String uploadFileAvatar(MultipartFile file) {
+        // Endpoint以华东1（杭州）为例，其它Region请按实际情况填写。
+        String endpoint = END_POINT;
+        // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
+        String accessKeyId = ACCESS_KEY_ID;
+        String accessKeySecret = ACCESS_KEY_SECRET;
+        // 填写Bucket名称，例如examplebucket。
+        String bucketName = ACCESS_NAME;
+        // 填写Object完整路径，例如exampledir/exampleobject.txt。Object完整路径中不能包含Bucket名称。
+//        String objectName = "exampledir/exampleobject.txt";
+//        // 填写本地文件的完整路径，例如D:\\localpath\\examplefile.txt。
+//        // 如果未指定本地路径，则默认从示例程序所属项目对应本地路径中上传文件流。
+//        String filePath= "D:\\localpath\\examplefile.txt";
+
+        try {
+            // 创建OSS实例。
+            OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+
+            //获取上传文件输入流
+            InputStream inputStream = file.getInputStream();
+            //获取文件名称
+            String fileName = file.getOriginalFilename();
+
+            //1 在文件名称里面添加随机唯一的值
+            String uuid = UUID.randomUUID().toString().replaceAll("-","");
+            //replaceAll把-转换为空格
+            // yuy76t5rew01.jpg
+            fileName = uuid+fileName;
+
+            //2 把文件按照日期进行分类
+            //获取当前日期
+            //   2019/11/12
+            String datePath = new DateTime().toString("yyyy/MM/dd");
+            //用到了joda-time工具类，转换日期格式
+            //拼接
+            //  2019/11/12/ewtqr313401.jpg
+            fileName = datePath+"/"+fileName;
+
+            //调用oss方法实现上传
+            //第一个参数  Bucket名称
+            //第二个参数  上传到oss文件路径和文件名称   aa/bb/1.jpg
+            //第三个参数  上传文件输入流
+            ossClient.putObject(bucketName,fileName , inputStream);
+
+            // 关闭OSSClient。
+            ossClient.shutdown();
+
+            //把上传之后文件路径返回
+            //需要把上传到阿里云oss路径手动拼接出来
+            //  https://edu-guli-1010.oss-cn-beijing.aliyuncs.com/01.jpg
+            String url = "https://"+bucketName+"."+endpoint+"/"+fileName;
+            return url;
+        }catch(Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
